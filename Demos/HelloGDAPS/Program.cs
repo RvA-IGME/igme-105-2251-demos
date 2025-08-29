50c67feed879d8a313ff4de204b662e3319a2b69
Erika Mesh <1838833+esmesh@users.noreply.github.com>
2025-08-29 12:21:06 -0400

    Initial commit

---
+++ Demos/HelloGDAPS/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Demo project created by Prof. Mesh so that
+ * everyone would have something in their new
+ * GitHub repo from the start. :)
+ */
+
+namespace HelloGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print some text to the console to be sure everything works
+            Console.WriteLine("Hello GDAPS1 student!");
+            Console.WriteLine("You successfully cloned and ran a project from GitHub!");
+        }
+    }
+}
---
+++ Demos/README_Demos.md
@@ -0,0 +1,3 @@
+# Demos folder
+Put demo projects provided to you or that you create for yourself here. You should also add comment to demo projects to help you study!
+
