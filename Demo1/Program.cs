2903fa3ad366679080b0c9f3bf47056380b5aaee
RvA-IGME <ra8532@rit.edu>
2025-09-03 10:47:34 -0400

    Added the Practice Exercise: Statements and Expressions.

---
+++ Demo1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Demo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(); // Spacing
+            Console.Write("We're testing a few things.");
+            Console.WriteLine("This line is 2 segments!");
+            Console.WriteLine();
+            Console.WriteLine(5 + 3 * 2 - 1);
+            Console.WriteLine(6.0 / 4 * 3 + 10);
+            Console.WriteLine(10 / 7 + 2 * 5);
+            Console.WriteLine();
+            Console.WriteLine("Goodbye now!");
+        }
+    }
+}
---
+++ PEs/PE1_statements_expressions/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace PE1_statements_expressions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name: " + "Rishi");
+            Console.WriteLine(); //Spacing            
+            Console.WriteLine("Vitality: " + 
+                (50 * 0.23)         );
+            Console.WriteLine("Armor: " + 
+                ((50 * 0.23) / 2)   );
+            Console.WriteLine("Agility: " + 
+                (7)                 );
+            Console.WriteLine("Defense: " + 
+                (((50 * 0.23) / 2) + 
+                7 - 
+                2)                  );
+            Console.WriteLine("Power: " + 
+            (50 -(
+                (50 * 0.23) +
+                ((50 * 0.23) / 2) +
+                7 +
+                ((50 * 0.23) / 2) + 7 - 2)
+            )                       );
+            Console.WriteLine();
+            Console.WriteLine("Stat Total: " + 50);
+        }
+    }
+}
