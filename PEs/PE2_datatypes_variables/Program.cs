20375ab24aa6306b1f26f9db7ff4ef4ad33e556d
RvA-IGME <ra8532@rit.edu>
2025-09-05 10:50:54 -0400

    Added Practice Exercise 2: Data types and Variables.

---
+++ PEs/PE2_datatypes_variables/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace PE2_datatypes_variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string charName;
+            int StatTotalPoints;
+            double statVitality;
+            double statArmor;
+            double statAgility;
+            double statDefense;
+            double statPower;
+
+            charName = "Rishi";
+            StatTotalPoints = 50;
+            statVitality = (StatTotalPoints * 0.23);
+            statArmor = (statVitality / 2);
+            statAgility = 7;
+            statDefense = (statArmor + statAgility) - 2;
+            statPower = (50 - statVitality - statArmor - statAgility - statDefense);
+
+            Console.WriteLine("Name: " + charName);
+            Console.WriteLine(); //Spacing   
+            Console.WriteLine("Vitality: " + statVitality);
+            Console.WriteLine("Armor: " + statArmor);
+            Console.WriteLine("Agility: " + statAgility);
+            Console.WriteLine("Defenses: " + statDefense);
+            Console.WriteLine("Power: " + statPower);
+            Console.WriteLine(); //Spacing   
+            Console.WriteLine("Stat Total: " + StatTotalPoints);
+
+        }
+    }
+}
