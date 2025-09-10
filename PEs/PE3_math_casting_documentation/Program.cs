158d34fefa6690fc0857b753a6e50faa3f2119e1
RvA-IGME <ra8532@rit.edu>
2025-09-10 10:49:44 -0400

    Added the variables of the PE, and the first part of the functioning.
    By my knowledge, everything matches the example, as it was meant to.

--- PEs/PE3_math_casting_documentation/Program.cs
+++ PEs/PE3_math_casting_documentation/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const double PI = 3.1415926535897931;
+            string playerName = "SirGalindo";
+
+            int Point1x = -13;
+            int Point1y = 51;
+
+            int Point2x = 17;
+            int Point2y = 28;
+
+            int hours = 274;
+
+            double numA = 7.9;
+            double numB = 2.25;
+
+            double angleDeg = 60;
+            double angleRad = (angleDeg * (PI / 180));
+
+            Console.WriteLine("Number A: " + numA);
+            Console.WriteLine("Number B: " + numB);
+            Console.WriteLine(numA + " + " + numB + " = " + (numA + numB));
+            Console.WriteLine("Now:");
+            Console.WriteLine((int)numA + " + " + (int)numB + " = " + ((int)numA + (int)numB));
+
+            Console.WriteLine();
+            Console.WriteLine();
         }
     }
 }
