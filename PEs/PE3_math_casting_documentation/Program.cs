671f0dc47c8e4b31a12e1572dd75a799add9d0bc
RvA-IGME <ra8532@rit.edu>
2025-09-10 10:08:21 -0400

    Implemented the Practice Exercise 'Casting, Math and Documentation'.
    (Nothing has been added at the present moment, but there will be shortly.

---
+++ PEs/PE3_math_casting_documentation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PE3_math_casting_documentation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
