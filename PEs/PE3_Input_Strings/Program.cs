eabe6340db316fb81b3c619e9c458bd44b980344
RvA-IGME <ra8532@rit.edu>
2025-09-19 10:48:02 -0400

    Amended certain capitalizations, and made the 2nd output sequence use ToUpper.

--- PEs/PE3_Input_Strings/Program.cs
+++ PEs/PE3_Input_Strings/Program.cs
@@ -22,8 +22,8 @@
             Console.Write("What is your pet's name? ");
 
             Console.ForegroundColor = ConsoleColor.Yellow;
-            string petName = Console.ReadLine();
-            petName = petName.Trim();
+            string PetName = Console.ReadLine();
+            PetName = PetName.Trim();
             Console.ForegroundColor = ConsoleColor.White;
 
             Console.Write("What is your favorite band? ");
@@ -34,11 +34,14 @@
             Console.ForegroundColor = ConsoleColor.White;
         Console.WriteLine(); //Spacing
             Console.WriteLine("Your name is "+Name.Length+" letters long,");
-            Console.WriteLine("which is "+(Name.Length - petName.Length)+" longer than "+petName+"'s name.");
+            Console.WriteLine("which is "+(Name.Length - PetName.Length)+" longer than "+PetName+"'s name.");
         Console.WriteLine(); //Spacing
-            Console.WriteLine("I wonder if " + petName + " likes " + BandName + " or the color " + Color + " too.");
+            string ColorUpper = PetName.ToUpper();
+            string PetNameUpper = PetName.ToUpper();
+            string BandNameUpper = PetName.ToUpper();
+            Console.WriteLine("I wonder if " + PetNameUpper + " likes " + BandNameUpper + " or the color " + ColorUpper + " too.");
         Console.WriteLine(); //Spacing
-            string ZanyName = Name.ToUpper()[0] + Color.ToLower()[..2] + petName.ToLower()[..2] + BandName.ToLower()[..2];
+            string ZanyName = Name.ToUpper()[0] + Color.ToLower()[..2] + PetName.ToLower()[..2] + BandName.ToLower()[..2];
             Console.WriteLine("Can I call you "+ZanyName+"? Or is that too on-the-nose?");
         }
     }
