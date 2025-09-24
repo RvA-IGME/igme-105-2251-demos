e9bcdbff05f56b2855865e9ca20798fb36db4db0
RvA-IGME <ra8532@rit.edu>
2025-09-24 10:20:07 -0400

    Added PE6: String Formatting

---
+++ PEs/PE6_string_formatting/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace PE6_string_formatting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string title;
+            string fullTitle;
+
+            double money;
+
+            
+            Console.Write("What is your name? ");
+            name = Console.ReadLine();
+            Console.Write("What is an adjective that describes you? ");
+            title = Console.ReadLine();
+
+            fullTitle = string.Format("{0} the {1}", name, title);
+            Console.WriteLine("Welcome, {0}!", fullTitle);
+            
+        }
+    }
+}
