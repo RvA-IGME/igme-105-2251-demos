6daa71f9346fec71e68562b6fcb804bac9701809
RvA-IGME <ra8532@rit.edu>
2025-09-26 10:47:08 -0400

    Added PE7: Boolean Expressions / If Statements.

---
+++ PEs/PE7-boolean-expressions/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace PE7_boolean_expressions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string target;
+
+            Console.Write("What does the golem detect?");
+            target = Console.ReadLine();
+            target = target.Trim();
+            target = target.ToLower();
+
+            if (target == "enemy")
+            {
+                Console.WriteLine("The golem takes aim and attacks.");
+            }
+            else
+            if (target == "wall")
+            {
+                Console.WriteLine("The golem observes the wall for a while, then turns back.");
+            }
+            else
+            if (target == "item")
+            {
+                Console.WriteLine("The golem observes the object, then picks it up.");
+            }
+            else 
+            if (target == "door")
+            {
+                Console.WriteLine("The golem opens the door rather gracelessly and enters.");
+            }
+
+            else
+            if (target == "shiro")
+            {
+                Console.WriteLine("The golem salutes the creature, and continues on.");
+            }
+
+            else
+            if (target.Length > 0)
+            {
+                Console.WriteLine("The golem does not act.");
+            }
+            else
+            {
+                Console.WriteLine("The golem remains dormant.");
+            }
+
+        }
+    }
+}
